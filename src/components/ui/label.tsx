5354147be55e8a2025b7daf8460b057d09d444bd
Shivam Sahu <51310713+seevam@users.noreply.github.com>
2025-10-08 23:48:58 +0530

    Update label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -14,13 +14,15 @@ const Label = React.forwardRef
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
     VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-))
+>(({ className, ...props }, ref) => {
+  return (
+    <LabelPrimitive.Root
+      ref={ref}
+      className={cn(labelVariants(), className)}
+      {...props}
+    />
+  )
+})
 Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
