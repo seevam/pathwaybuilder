93ff12bee97f1f56c0516a72a9c273a77a51b92e
Shivam Sahu <51310713+seevam@users.noreply.github.com>
2025-10-11 06:24:34 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,10 +15,9 @@ export default clerkMiddleware((auth, request) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-      '/[moduleSlug]/:path*',
   ],
 }
